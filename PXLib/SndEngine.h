9e4a21b19a2aa7f644cea5681a16cad991afa555
dark.ying <dark.ying@38837db2-ba3d-11de-9a73-11f8458f6c97>
2009-11-24 11:51:48 +0000

    Ying: SndEngine

--- PXLib/SndEngine.h
+++ PXLib/SndEngine.h
@@ -1,8 +1,19 @@
 #pragma once
 
+#include "System.h"
+
+class Timer; 
+class SndPart;
+
 class SndEngine
 {
 public:
 	SndEngine(void);
 	~SndEngine(void);
+
+	void Enter(Timer * timer);
+	void Update();
+	void Exit();
+
+	SndPart* GeneratePart();
 };
