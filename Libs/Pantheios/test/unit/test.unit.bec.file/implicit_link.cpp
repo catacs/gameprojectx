1059c007a9be29795e5413f871da817e13084b6f
dark.ying <dark.ying@38837db2-ba3d-11de-9a73-11f8458f6c97>
2010-02-21 20:30:02 +0000

    Ying: Added new Log library (pantheios)

---
+++ Libs/Pantheios/test/unit/test.unit.bec.file/implicit_link.cpp
@@ -0,0 +1,34 @@
+/* /////////////////////////////////////////////////////////////////////////
+ * File:        implicit_link.cpp
+ *
+ * Purpose:     Implicit link file for the test.unit.bec.file project.
+ *
+ * Created:     2nd August 2009
+ * Updated:     2nd August 2009
+ *
+ * Status:      Wizard-generated
+ *
+ * License:     (Licensed under the Synesis Software Open License)
+ *
+ *              Copyright (c) 2008-2009, Synesis Software Pty Ltd.
+ *              All rights reserved.
+ *
+ *              www:        http://www.synesis.com.au/software
+ *
+ * ////////////////////////////////////////////////////////////////////// */
+
+
+/* /////////////////////////////////////////////////////////////////////////
+ * Includes
+ */
+
+/* Pantheios Header Files */
+#include <pantheios/implicit_link/bec.file.h>
+
+/* xTests Header Files */
+#include <xtests/implicit_link.h>
+
+/* shwild Header Files */
+#include <shwild/implicit_link.h>
+
+/* ///////////////////////////// end of file //////////////////////////// */
